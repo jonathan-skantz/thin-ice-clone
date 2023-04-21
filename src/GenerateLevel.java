c6784cfac2fe573d674a9b640223c57942c472f9
hollstra <hollstra@kth.se>
2023-04-21 10:44:10 +0200

    Add code skeleton for generating a matrix representation for a randomized level

---
+++ src/GenerateLevel.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+import java.util.Stack;
+
+public class GenerateLevel {
+
+    private int[][] board = new int[19][15];
+    Random rand = new Random();
+
+    public GenerateLevel() {
+        // Assign all blocks as walls
+        for(int i = 0; i < 19; i++) {
+            for(int j = 0; j < 15; j++) {
+                board[i][j] = 1;
+            }
+        }
+    }
+
+    /**
+     * Generate a randomized level
+     * 
+     * @return a matrix representation of the level
+     */
+    public int[][] generate() {
+        return recursiveBacktracking();
+    }
+
+    /**
+     * Perform Recursive Backtracking algorithm on game board to randomize wall placement
+     * 
+     * @return a matrix representation of the level
+     */
+    public int[][] recursiveBacktracking() {
+        // TODO Implement maze algorithm 
+    }
+}
