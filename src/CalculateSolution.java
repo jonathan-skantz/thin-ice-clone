7b24c3d72367e9dc6586bfc3b4d2e6a85e5341e9
hollstra <hollstra@kth.se>
2023-04-26 16:43:11 +0200

    Improved CalculateSolution but still not correct

--- src/CalculateSolution.java
+++ src/CalculateSolution.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class CalculateSolution {
-    private static int[][] maze;
+    private int[][] maze;
     private boolean[][] visited;
     private int dimension;
     private Node startNode;
@@ -12,13 +12,15 @@ public class CalculateSolution {
      *
      * @param maze a 2D array representing the maze
      */
-    CalculateSolution(int[][] maze, Node start, Node end) {
-        CalculateSolution.maze = maze;
+    CalculateSolution(int[][] maze, Node startNode, Node endNode) {
+        this.maze = maze;
         this.dimension = maze.length;
         this.visited = new boolean[dimension][dimension];
-        this.startNode = start;
-        this.endNode = end;
+        this.startNode = startNode;
+        this.endNode = endNode;
     }
+
+    
 
     /**
      * Calculates the path to solve the maze using depth-first search.
@@ -39,7 +41,7 @@ public class CalculateSolution {
                 return path;
             }
 
-            for (Node neighbor : findNeighbors(currNode)) {
+            for (Node neighbor : getNeighbors(currNode)) {
                 if (!visited[neighbor.y][neighbor.x]) {
                     stack.push(neighbor);
                     visited[neighbor.y][neighbor.x] = true;
@@ -51,56 +53,29 @@ public class CalculateSolution {
     }
 
     /**
-     * Helper method that checks if a given point is on the grid
-     * 
-     * @param x x-coordinate
-     * @param y y-coordinate
-     * @return true if the point is on the grid, false otherwise
-     */
-    private Boolean pointOnGrid(int x, int y) {
-        return x >= 0 && y >= 0 && x < dimension && y < dimension;
-    }
-
-    /**
-     * Helper method that checks if a given point is not diagonally adjacent to a given node
-     * 
-     * @param node the node to compare against
-     * @param x x-coordinate
-     * @param y y-coordinate
-     * @return true if the point is not diagonally adjacent to the given node, false otherwise
-     */
-    private Boolean pointNotCorner(Node node, int x, int y) {
-        return (x == node.x || y == node.y);
-    }
-
-    /**
-     * Helper method that checks that a given node is not already part of the maze
-     * 
-     * @param node the node to compare against
-     * @param x x-coordinate
-     * @param y y-coordinate
-     * @return true if the point is not the same as the given node, false otherwise
-     */
-    private Boolean pointNotNode(Node node, int x, int y) {
-        return !(x == node.x && y == node.y);
-    }
-
-    /**
-     * Finds the neighbors of a given node.
+     * Returns an ArrayList of neighboring nodes that are valid next steps.
      *
-     * @param node the node to find neighbors for
+     * @param node the current node
      * @return an ArrayList of neighboring nodes
      */
-    private ArrayList<Node> findNeighbors(Node node) {
+    private ArrayList<Node> getNeighbors(Node node) {
         ArrayList<Node> neighbors = new ArrayList<>();
-        for (int y = node.y-1; y < node.y+2; y++) {
-            for (int x = node.x-1; x < node.x+2; x++) {
-                if (pointOnGrid(x, y) && pointNotCorner(node, x, y) && pointNotNode(node, x, y)) {
-                    neighbors.add(new Node(x, y));
-                }
-            }
+        int x = node.x;
+        int y = node.y;
+
+        if (x > 0 && maze[y][x-1] == 1) {
+            neighbors.add(new Node(x-1, y));
         }
+        if (x < dimension-1 && maze[y][x+1] == 1) {
+            neighbors.add(new Node(x+1, y));
+        }
+        if (y > 0 && maze[y-1][x] == 1) {
+            neighbors.add(new Node(x, y-1));
+        }
+        if (y < dimension-1 && maze[y+1][x] == 1) {
+            neighbors.add(new Node(x, y+1));
+        }
+
         return neighbors;
     }
-
 }
--- src/Node.java
+++ src/Node.java
@@ -5,6 +5,7 @@ public class Node {
    
     public final int x;
     public final int y;
+    public Node prev;
 
     /**
      * Constructor for Node class.
@@ -15,6 +16,7 @@ public class Node {
     Node(int x, int y) {
         this.x = x;
         this.y = y;
+        this.prev = null;
     }
     
 }
